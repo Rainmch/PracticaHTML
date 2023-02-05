ac9b40e0a0ea3c376dcfc16749d9c0708fba49be
Jorge Gomez <jorgegomezrios@gmail.com>
2023-02-05 14:29:26 -0600

    Primera version, aun se resetea el resultado

---
+++ script.js
@@ -0,0 +1,6 @@
+function suma(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    document.getElementById("resultado").innerHTML = num1 + num2;
+    
+}
