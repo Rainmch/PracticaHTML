7b7bdda5df6c1969b88efe15b4c3b9564d52608c
Jorge Gomez <jorgegomezrios@gmail.com>
2023-02-05 14:43:37 -0600

    Se agrego la funcion preventDefault y la pagina funciona en su totalidad
    Z
    Z

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function suma(){
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     document.getElementById("resultado").innerHTML = num1 + num2;
-    
+    event.preventDefault();
 }
